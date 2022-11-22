3171adc5acde35521043c0ccd98c8ceb6de01e28
aziz-codes <azizisoffline@gmail.com>
2022-11-22 21:07:19 +0500

    folder structure and pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/components/**/*.{js,jsx}",
+    "./src/pages/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
